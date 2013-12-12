7f321e498c5657837f0b339b016cda72d40dae0a
Infinite Flux Master <braungoodson@gmail.com>
2013-12-11 23:24:42 -0500

    added source

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node 
 
+var spawn = require('child_process').spawn;
+var bashcoin = spawn('./node_modules/bashcoin/bashcoin.js',['-c','-A']);
+var neuralnet = spawn('./node_modules/neuralnet/neuralnet.js');
+
+bashcoin.stdout.on('data',function(d){
+	process.stdout.write(d);
+});
+bashcoin.stderr.on('data',function(d){
+	process.stdout.write(d);
+});
+bashcoin.on('close',function(d){
+	process.stdout.write(d);
+});
+
+/*
 var cp = require('child_process');
 var n = cp.fork(__dirname+'/node_modules/bashcoin/bashcoin.js',['-c','-A']);
-
+*/
 
 /*
 
