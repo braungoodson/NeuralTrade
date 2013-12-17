58277e13383c7ed10818870d559eb53f276bdf7e
braun <braun@brauns-iMac.local>
2013-12-16 19:48:34 -0500

    added request

--- btc-e.js
+++ btc-e.js
@@ -1,8 +1,60 @@
+/*
+
+var https = require('https');
+https.get('https://btc-e.com/api/2/btc_usd/ticker',function(r){
+	//console.log(' \033[33mresult: \033[0m'+r);
+	r.on('data',function(d){
+		process.stdout.write(' \033[34mstream:\033[0m '+d);
+	});
+}).on('error',function(r){
+	console.log(' \033[320mError: '+e);
+});
+
+
+*/
+
+/*
+
+
+var https = require('https');
+var q = https.request({
+	hostname: 'btc-e.com',
+	port: 443,
+	path: '/api/2/btc_usd/ticker',
+	method: 'GET' 
+},function(r){
+	r.on('data',function(d){
+		console.log(d);
+	});
+});
+
+*/
+
+/*
+
+// curl -k https://localhost:8000/
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('./brauns-key.pem'),
+  cert: fs.readFileSync('./brauns-cert.pem')
+};
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(8000);
+
+*/
+
+
+/*
 var http = require('http');
 http.get('http://btc-e.com/api/2/btc_usd/ticker',function(r){
 	console.log(r);
 }).on('error',function(e){throw e;});
-
+*/
 /*
 module.exports = function () {
 	var http = require('http');
