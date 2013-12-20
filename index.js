913acff555cf6145b0687a48f1bd90dcfcb4dc53
braun <braun@brauns-iMac.local>
2013-12-20 01:17:56 -0500

    installed trade-api

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var key = 'BB8TIFKG-MW2GOGLE-1G60QKNL-DTKWTISA-TR71OI8C';
 var secret = '465cec0c07565b19829184809de5bb291c791cd00fc63a03c2f96ead01ec4fab';
 var b = require('btc-e');
 //b(PORT,BUYIN,CASH,BTC,CHIP,FUTURE);
-b(process.pid,10001,9.9,9.9,0,.005,10000,key,secret);
+b(process.pid,13000,0,0,0,.01,10000,key,secret);
