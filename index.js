1baae75e42cbd07d75a61e79a248bb8e3f6b21dc
Infinite Flux Master <braungoodson@gmail.com>
2013-12-22 16:56:51 -0500

    ok

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var key = '';
-var secret = '';
+var key = '4UD2ZTH0-JETMWFNR-XGMANWA6-WYAHXJX6-FIMHM5RD';
+var secret = 'ecb2f7377ab67d75f80474839e304707070c34b07f4d8d23fc84961c500904cd';
 var b = require('btc-e');
 //b(PORT,BUYIN,CASH,BTC,CHIP,FUTURE);
 b(process.pid,10000,0,0,0,.01005,10000,key,secret);
